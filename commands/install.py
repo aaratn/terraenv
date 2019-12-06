76a08050195a0ab0f3bd71ca5e9ca8140e3daebe
piyush <pjsonigra@gmail.com>
2019-12-06 21:53:10 +0530

    separate linux build with circleci, add dockerfile and handle permission errro

--- commands/install.py
+++ commands/install.py
@@ -20,33 +20,33 @@ def download_program(program, version):
     if program == "terraform":
         url = "https://releases.hashicorp.com" + "/terraform/" + version + \
             "/terraform_" + version + "_" + operating_sys + "_amd64.zip"
-    
+
     elif program == "terragrunt":
         url = "https://github.com/gruntwork-io/terragrunt/releases/download/v" + \
             version + "/terragrunt_" + operating_sys + "_amd64"
-    
+
     if not os.path.exists(DOWNLOAD_PATH + program + "_" + version):
 
         print("Downloading", program, version, "from", url)
 
         binary = requests.get(url)
-        
+
         if binary.status_code == 404:
             raise Exception("Invalid version, got 404 error !")
-    
+
         dest_path = DOWNLOAD_PATH + program + "_" + version
 
         open(dest_path, 'wb').write(binary.content)
 
         if program == "terraform":
-            
+
             with ZipFile(dest_path, 'r') as zip:
                 zip.extract('terraform', path=DOWNLOAD_PATH)
 
             if os.path.exists(DOWNLOAD_PATH + '/' + program) and os.path.exists(dest_path):
                 os.remove(dest_path)
                 os.rename(DOWNLOAD_PATH + '/' + program, dest_path)
-            
+
             else:
                 raise Exception("Issue extracting terraform !!")
 
@@ -58,35 +58,41 @@ def download_program(program, version):
 
 
 def install(args):
-    
+
     program = args.program
     version = args.version
 
     if not version and os.path.exists(VERSION_FILE):
         load_dotenv(dotenv_path=VERSION_FILE)
         version = (os.getenv(program.upper()))
-    
+
     if not version:
         print ("Please define version or add that to .terraenv file")
         sys.exit(1)
 
 
     dest_path = DOWNLOAD_PATH + program + "_" + version
-    
+
     if program == "terraform":
         download_program(program, version)
-    
+
     elif program == "terragrunt":
         download_program(program, version)
-    
+
     else:
         raise Exception(
             'Invalid Arguement !! It should be either terraform / terragrunt')
-    
+
+    if not os.access('/usr/local/bin', os.W_OK):
+        print("Error: User doesn't have write permission of /usr/local/bin directory.\
+            \n\nRun below command to grant permission and rerun 'terraenv install' command.\
+            \nsudo chown -R $(whoami) /usr/local/bin\n")
+        sys.exit(1)
+
     try:
         os.remove("/usr/local/bin/" + program )
-    
+
     except FileNotFoundError:
         pass
-    
+
     os.symlink(dest_path, "/usr/local/bin/" + program )
